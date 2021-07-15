d58043fd6a64a70826a4e697c8d7d38ac53a2059
Eivind Skjelmo <eivind@kodeworks.no>
2021-07-15 14:40:34 +0200

    Add logo and logo symbol to header

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,14 @@
 const Header = ({children}) => {
   return (
     <header>
-      Kodeworks header
+      <div className="relative flex items-center justify-center isolate m-28">
+        <img src="/Logo.svg" alt="Kodeworks Logo" className="h-12 ml-5 z-10" />
+        <img
+          src="/Logo-symbol.svg"
+          alt="Kodeworks Logo"
+          className="h-64 ml-5 absolute z-0"
+        />
+      </div>
       {children}
     </header>
   )
