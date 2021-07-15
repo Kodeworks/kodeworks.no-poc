6a170ee6ecea637a8ca8dfe77f9ed76a0c078c39
Eivind Skjelmo <eivind@kodeworks.no>
2021-07-15 14:42:03 +0200

    Style address in footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = ({children}) => {
   return (
     <footer className="flex items-center justify-center w-full h-24 border-t">
-      <address className="mt-3 mb-2 sm:mt-2 sm:mb-0 flex flex-row space-x-5 items-center">
+      <address className="flex flex-row items-center mt-3 mb-2 space-x-5 sm:mt-2 sm:mb-0">
         <div className="text-sm not-italic text-left text-grey-700">
           Fjordgata 30 <br />
           7010 Trondheim <br />
@@ -13,7 +13,7 @@ const Footer = ({children}) => {
           <a href="mailto:post@kodeworks.no">post@kodeworks.no</a>
         </div>
       </address>
-      <img src="/logo.svg" alt="Kodeworks Logo" className="h-4 ml-5" />
+      <img src="/Logo.svg" alt="Kodeworks Logo" className="h-4 ml-5" />
       {children}
     </footer>
   )
