8914451a2b0d339afa466d05d84e1058ddca2b79
Eivind Skjelmo <eivind@kodeworks.no>
2021-07-16 15:51:55 +0200

    Add seismograph with logo

--- components/Seismograph.js
+++ components/Seismograph.js
@@ -1,10 +1,11 @@
 const Seismograph = ({children}) => {
   return (
-    <div className="flex justify-center">
+    <div className="relative flex justify-center">
       <img
         src="/seismograph.svg"
         alt="Generative art looking like a seismogram based on digital interactions in Kodeworks"
       />
+      {children}
     </div>
   )
 }
