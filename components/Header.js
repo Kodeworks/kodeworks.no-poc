73319661b95c039b6766344d8f8453ea312242c6
Eivind Skjelmo <eivind@kodeworks.no>
2021-07-16 15:43:38 +0200

    Replace header logo with menu mock

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,7 @@
 const Header = ({children}) => {
   return (
-    <header>
-      <div className="relative flex items-center justify-center isolate m-28">
-        <img src="/Logo.svg" alt="Kodeworks Logo" className="h-12 ml-5 z-10" />
-        <img
-          src="/Logo-symbol.svg"
-          alt="Kodeworks Logo"
-          className="h-64 ml-5 absolute z-0"
-        />
-      </div>
+    <header className="fixed top-0 right-0">
+      <button className="p-6 text-2xl hover:underline">Meny</button>
       {children}
     </header>
   )
