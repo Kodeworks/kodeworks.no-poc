e78dbb4661bd65310d862b5a9215c00b316056d8
Eivind Skjelmo <eivind@kodeworks.no>
2021-07-16 15:48:09 +0200

    Consider disabling utilities for faux wont weights

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,20 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    /*
+      Consider disabling font weights not supported by custom font to avoid faux font weight
+    */
+    fontWeight: {
+      thin: 100,
+      extralight: 200, // faux font weight
+      light: 300,
+      normal: 400,
+      medium: 500,
+      semibold: 600, // faux font weight
+      bold: 700,
+      extrabold: 800, // faux font weight
+      black: 900, // faux font weight
+    },
     extend: {
       fontFamily: {
         'primary': ['"NON Natural Grotesk"', 'Arial', 'sans-serif'],
