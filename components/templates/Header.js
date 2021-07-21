32d8b8fd631c07355e9ff5be7fb4ec5829a73581
Eivind Skjelmo <eivind@kodeworks.no>
2021-07-21 10:51:41 +0200

    Add z-indeks to header

--- components/templates/Header.js
+++ components/templates/Header.js
@@ -1,7 +1,9 @@
 const Header = ({children}) => {
   return (
-    <header className="fixed top-0 right-0">
-      <button className="p-6 text-2xl hover:underline">Meny</button>
+    <header className="fixed top-0 right-0 z-50">
+      <button className="p-6 text-2xl bg-opacity-50 bg-main-gray-500 hover:underline">
+        Meny
+      </button>
       {children}
     </header>
   )
