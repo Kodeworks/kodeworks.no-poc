45ec60e1488d2b69366c4350fd3f4180c6009d59
Eivind Skjelmo <eivind@kodeworks.no>
2021-07-21 11:01:37 +0200

    Replace ImageSection with CardComponent

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Layout from '../components/templates/Layout'
 import Seismograph from '../components/molecules/Seismograph'
 import Logo from '../components/atoms/Logo'
-import ImageSection from '../components/templates/ImageSection'
 import CardContainer from '../components/molecules/CardContainer'
 import Variant from '../components/atoms/Variant'
 import LinkButton from '../components/molecules/LinkButton'
@@ -19,7 +18,33 @@ export default function Home() {
       </Seismograph>
       <h1>Hvem er vi?</h1>
 
-      <ImageSection></ImageSection>
+      <section className="relative my-16">
+        <CardContainer variant={Variant.BLACK}>
+          <div className="md:mr-auto md:w-1/2 md:pr-10">
+            <h2 className="mt-2 text-3xl font-normal tracking-tight text-white sm:text-4xl">
+              Vi muliggjør ideer!
+            </h2>
+            <p className="mt-3 text-lg">
+              Vi er en gjeng som gjør det mulig for deg å gjennomføre ideer.
+              Hvordan du gjør det, bestemmer du helt selv!
+            </p>
+            <div className="mt-8">
+              <LinkButton
+                variant={Variant.WHITE}
+                linkText="Personalhåndboka"
+                url="#"
+              ></LinkButton>
+            </div>
+          </div>
+        </CardContainer>
+        <div className="relative h-56 bg-transparent sm:h-72 md:absolute md:right-0 md:top-0 md:h-full md:w-1/2">
+          <img
+            className="object-cover w-full h-full"
+            src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60"
+            alt=""
+          />
+        </div>
+      </section>
       <section className="my-16">
         <CardContainer variant={Variant.BLACK}>
           <div className="md:mr-auto md:w-1/2 md:pr-10">
