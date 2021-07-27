6d40225571c5d4019dc8d4a3c09a83e5da192898
Eivind Skjelmo <eivind@kodeworks.no>
2021-07-27 11:13:42 +0200

    Add initial data on people and projects

---
+++ data/people.js
@@ -0,0 +1,42 @@
+import {projectNames} from './projects'
+
+const people = [
+  {
+    id: '1',
+    portrait: '/portraits/silouette.png',
+    name: 'Ola Nordmann',
+    projects: [projectNames.INATUR, projectNames.EQUINOR],
+  },
+  {
+    id: '2',
+    portrait: '/portraits/silouette.png',
+    name: 'Kari Nordmann',
+    projects: [projectNames.INATUR],
+  },
+  {
+    id: '3',
+    portrait: '/portraits/silouette.png',
+    name: 'Henning Olsen',
+    projects: [projectNames.INATUR],
+  },
+  {
+    id: '4',
+    portrait: '/portraits/silouette.png',
+    name: 'Ola Nordmann',
+    projects: [projectNames.INATUR],
+  },
+  {
+    id: '5',
+    portrait: '/portraits/silouette.png',
+    name: 'Kari Nordmann',
+    projects: [projectNames.INATUR],
+  },
+  {
+    id: '6',
+    portrait: '/portraits/silouette.png',
+    name: 'Henning Olsen',
+    projects: [projectNames.INATUR],
+  },
+]
+
+export default people
---
+++ data/projects.js
@@ -0,0 +1,22 @@
+export const projectNames = {
+  INATUR: 'Inatur',
+  VASKEHJELP: 'Vaskehjelp',
+  EQUINOR: 'Equinor',
+}
+
+const projects = [
+  {
+    id: '1',
+    name: projectNames.INATUR,
+  },
+  {
+    id: '2',
+    name: projectNames.VASKEHJELP,
+  },
+  {
+    id: '3',
+    name: projectNames.EQUINOR,
+  },
+]
+
+export default projects
