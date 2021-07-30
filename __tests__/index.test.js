53813ffefcfae8e244a3b04653739a154a6c8602
Eivind Skjelmo <eivind@kodeworks.no>
2021-07-30 10:19:02 +0200

    Restructure test files

--- __tests__/index.test.js
+++
@@ -1,6 +0,0 @@
-import {render, screen} from '@testing-library/react'
-import Home from 'pages/index'
-
-test('renders page', async () => {
-  render(<Home />)
-})
---
+++ tests/__tests__/pages/index.test.js
@@ -0,0 +1,16 @@
+import {render, screen} from '@testing-library/react'
+import Home from 'pages/index'
+
+test('renders page', async () => {
+  render(<Home />)
+})
+
+test('h1 with SVG as title should contain screen-reader friendly title', async () => {
+  const {container} = render(<Home />)
+  const screenReaderTitle = screen.getByText('Kodeworks')
+
+  expect(screenReaderTitle.tagName).toEqual('SPAN')
+  expect(screenReaderTitle.classList.contains('sr-only')).toBe(true)
+  expect(screenReaderTitle.parentElement.tagName).toEqual('H1')
+  expect(screenReaderTitle.nextSibling.tagName).toEqual('svg')
+})
