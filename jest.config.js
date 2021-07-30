f185c54af6f74353ada865504fe2de47637d9294
Eivind Skjelmo <eivind@kodeworks.no>
2021-07-30 08:28:17 +0200

    Configure babel and Jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+  moduleDirectories: ['node_modules', __dirname],
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+}
