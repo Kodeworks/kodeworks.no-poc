06b257404d89b9c5f8968f1539e31d4ed3dd23ca
Eivind Skjelmo <eivind@kodeworks.no>
2021-08-02 16:05:10 +0200

    Update project info

--- data/projects.js
+++ data/projects.js
@@ -12,14 +12,15 @@ const projects = [
     name: projectNames.INATUR,
     technologies: [tech.JAVA, tech.REACT, tech.WEBPACK, tech.MONGODB],
     illustration: {
-      src: 'https://images.unsplash.com/photo-1548795179-8bfc5c1ebb24?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
-      altText: 'Cabin with a fjord and high mountains in the background',
-      tailwindAspectRatio: 'aspect-w-2 aspect-h-1',
+      src: 'https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2700&q=80',
+      altText:
+        'View from a boat on a lake with high mountains in the background',
+      tailwindAspectRatio:
+        'aspect-w-5 aspect-h-2 md:aspect-w-6 md:aspect-h-2 lg:aspect-w-4 lg:aspect-h-1',
     },
     input: [
       'Inatur er Norges største markedsplass for jakt, fiske og hytter i norsk natur.',
       'Nytt avsnitt med mer om utgangspunktet for prosjektet.',
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vulputate lectus eu maximus tristique. Etiam ut dolor nec tellus pellentesque fringilla id nec orci. Ut eu est tristique, dapibus nibh vel, iaculis nisi. Nam ac pharetra ligula. Sed convallis, mi a efficitur lacinia, velit elit tincidunt enim, vitae scelerisque odio tortor mollis mauris. Nam dignissim, nibh eu suscipit molestie, ex lorem egestas est, eu ultricies nibh leo nec nulla. Donec in nisi nunc. Proin vel turpis id lorem tristique pharetra vel et quam. Pellentesque a tellus sit amet ligula aliquet feugiat at in erat. Cras convallis, urna sed molestie scelerisque, velit dolor dapibus tellus, nec fringilla nunc felis tempus mauris. Duis at tempus sem. In hac habitasse platea dictumst. Donec vitae leo fringilla, efficitur sem ut, congue magna. Etiam in finibus orci. Aliquam euismod sodales erat sit amet porttitor. Nam a accumsan turpis.',
     ],
     output: [
       'Kodeworks videreutvikler og vedlikeholder løsninger for Inatur.',
@@ -31,9 +32,10 @@ const projects = [
     name: projectNames.VASKEHJELP,
     technologies: [tech.CSHARP, tech.BLAZOR, tech.TAILWINDCSS],
     illustration: {
-      src: 'https://images.unsplash.com/photo-1520509414578-d9cbf09933a1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
-      altText: 'Computer screen with programming code',
-      tailwindAspectRatio: 'aspect-w-9 aspect-h-7 md:aspect-w-2 md:aspect-h-3',
+      src: 'https://images.unsplash.com/photo-1600585152220-90363fe7e115?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80',
+      altText: 'Clean kitchen',
+      tailwindAspectRatio:
+        'aspect-w-5 aspect-h-2 md:aspect-w-6 md:aspect-h-2 lg:aspect-w-4 lg:aspect-h-1',
     },
     input: [
       'Vaskehjelp er en markedsplass som kobler vasker og kunder sammen. Vaskehjelps rolle er å tilby plattformen hvor rengjører og kunde kan finne hverandre, samt ta av seg betaling og annet papirarbeid.',
@@ -47,16 +49,17 @@ const projects = [
     name: projectNames.EQUINOR,
     technologies: [tech.CSHARP, tech.AZURE, tech.MYSQL],
     illustration: {
-      src: 'https://images.unsplash.com/photo-1605647540924-852290f6b0d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60',
-      altText: 'Cabin with a fjord and high mountains in the background',
-      tailwindAspectRatio: 'aspect-w-5 aspect-h-2 md:aspect-w-6 md:aspect-h-2',
+      src: 'https://images.unsplash.com/photo-1605647540924-852290f6b0d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1600&q=80',
+      altText: 'Person wearing a VR headset',
+      tailwindAspectRatio:
+        'aspect-w-5 aspect-h-2 md:aspect-w-6 md:aspect-h-2 lg:aspect-w-4 lg:aspect-h-1',
     },
     input: [
       'Equinor supplerer 170 millioner mennesker med energi daglig, og har de siste årene iverksatt en strategisk satsing på bærekraftig energi. Dette er en prosess som krever innovasjon, forretningsforståelse og et stort fokus på kvalitet. ',
       'Nytt avsnitt med mer om utgangspunktet for prosjektet.',
     ],
     output: [
-      'Kodeworks støtter Equinors forsknings- og digitaliseringsaktiviteter gjennom analyse, design, programmering og testing.',
+      'Kodeworks støtter Equinors forsknings- og digitaliseringsaktiviteter gjennom analyse, design, programmering og testing. Etiam ut dolor nec tellus pellentesque fringilla id nec orci. Ut eu est tristique, dapibus nibh vel, iaculis nisi.',
     ],
   },
 ]
